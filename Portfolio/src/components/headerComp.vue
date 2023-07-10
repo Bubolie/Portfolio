3b7678b324dbe9e5da1443897fa2c944a457135d
Bubolie <elodie41.m@gmail.com>
2023-07-10 17:11:38 +0300

    creation de l'aboresence des fichiers et dans header, ajout de la photo et la barre de navigation

---
+++ Portfolio/src/components/headerComp.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+
+</script>
+
+<template>
+  <header>
+    <img alt="Photo d'identité d'une femme de type occidental, blonde aux yeux bleu" class="photo" src="../assets/image/photoProfil.png" width="125" height="125" />
+      <nav>
+        <ul>
+          <li><RouterLink to="/">Je me présente...</RouterLink></li>
+          <li><RouterLink to="/creations">Mes Créations</RouterLink></li>
+          <li><RouterLink to="/contact">Me contacter ? Par ici...</RouterLink></li>
+        </ul>
+        
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
