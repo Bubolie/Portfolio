46269b428be184b0d5b85e89c0a41a5b1ff12a92
Bubolie <elodie41.m@gmail.com>
2023-07-19 19:03:39 +0300

    changement de la mise en page, debut de création du modal avec le bouton sans mise en forme

--- Portfolio/src/components/creation.vue
+++ Portfolio/src/components/creation.vue
@@ -1,30 +1,90 @@
-<script setup></script>
+<script setup>
+import Modal from "../components/modal.vue";
+import { ref } from "vue";
+
+const showModal = ref(false);
+</script>
 
 <template>
   <section id="project">
     <h1>Mes projets</h1>
     <section class="creation">
-      <img class="creation__image" src="../assets/image/imageCV.png" alt="Image dun CV"/>
+      <img
+        class="creation__image"
+        src="../assets/image/imageCV.png"
+        alt="Image dun CV"
+      />
       <div class="creation__info">
         <h2 class="creation__title">Curriculum Vitae</h2>
-        <button class="creation__moreDetail">Plus de détails</button>
+        
+        <div id="button-modal">
+          <button class="creation__moreDetail" @click="showModal = true">
+            Plus de détail
+          </button>
+          <Teleport to="body">
+            <modal :show="showModal" @close="showModal = false"
+            title="Curriculum Vitae"
+            lang="HTML et CSS"
+            logiciel="Virtual Studio Code"
+            date="30 mai 2023"
+            link="Voir sur GitHub : ici"
+            >
+            </modal>
+          </Teleport>
+        </div>
       </div>
     </section>
 
     <section class="creation">
-      <img class="creation__image" src="../assets/image/imageCDC.png" alt="Image de la page d'acceuil et sommaire d'un cahier des Cahrge pour l'entreprise La Socketterie"/>
+      <img
+        class="creation__image"
+        src="../assets/image/imageCDC.png"
+        alt="Image de la page d'acceuil et sommaire d'un cahier des Cahrge pour l'entreprise La Socketterie"
+      />
       <div class="creation__info">
         <h2 class="creation__title">Cahier des Charges</h2>
-        <button class="creation__moreDetail">Plus de détails</button>
+        <div id="button-modal">
+          <button class="creation__moreDetail" @click="showModal = true">
+            Plus de détail
+          </button>
+          <Teleport to="body">
+            <modal :show="showModal" @close="showModal = false"
+            title="Cahier des Charges"
+            logiciel="Word"
+            date="11 juin 2023"
+            link="Voir un extrait du PDF ici"
+            >
+            </modal>
+          </Teleport>
+        </div>
+        <div></div>
       </div>
     </section>
 
     <section class="creation">
-      <img class="creation__image" src="../assets/image/imageEspace Com.png" alt="Image d'un espace de commentaires"/>
+      <img
+        class="creation__image"
+        src="../assets/image/imageEspace Com.png"
+        alt="Image d'un espace de commentaires"
+      />
       <div class="creation__info">
         <h2 class="creation__title">Espace de commentaires</h2>
-        <button class="creation__moreDetail">Plus de détails</button>
-      </div>
+        
+        <div id="button-modal">
+          <button class="creation__moreDetail" @click="showModal = true">
+            Plus de détail
+          </button>
+          <Teleport to="body">
+            <modal :show="showModal" @close="showModal = false"
+            title="Espace de commentaires"
+            lang="HTML, CSS et JavaScript"
+            logiciel="Virtual Studio Code"
+            date="30 juin 2023"
+            link="Voir sur GitHub : ici"
+            >
+            </modal>
+          </Teleport>
+        </div>      </div>
     </section>
   </section>
 </template>
@@ -36,12 +96,13 @@
   border-bottom: solid 6px #400738;
   border-top: solid 6px #400738;
 }
+
 .creation {
-  margin: 20px 100px;
-  padding: 30px;
+  margin-left: 100px;
+  margin-right: 100px;
+  padding: 50px;
   border-top: solid 3px #400738;
   display: flex;
-  margin: 30px;
   align-items: center;
   justify-content: center;
 }
@@ -61,15 +122,17 @@
 }
 
 .creation__moreDetail {
-border-radius: 20px;
-border: solid 3px;
-padding: 10px;
-cursor: pointer;
-background-color:#F9F0E3;
-color: #400738;
+  border-radius: 20px;
+  border: solid 3px;
+  padding: 10px;
+  cursor: pointer;
+  background-color: #f9f0e3;
+  color: #400738;
 }
+
 .creation__moreDetail:hover {
-background-color: #400738;
-color: #F9F0E3;
+  background-color: #400738;
+  color: #f9f0e3;
 }
+
 </style>
--- Portfolio/src/components/modal.vue
+++ Portfolio/src/components/modal.vue
@@ -1,17 +1,91 @@
 <template>
+  <Transition name="modal">
+    <div v-if="show" class="modal__mask">
+      <div class="modal__container">
+        <div class="modal__header">
+          <slot name="title">{{title}}</slot>
+        </div>
 
+        <div class="modal__body">
+          <slot name="lang">Langages utilisés : {{ lang }}</slot>
+        </div>
+        <div class="modal__body">
+          <slot name="date">Date de création : {{ date }}</slot>
+        </div>
+        <div class="modal__body">
+          <slot name="logiciel">Logiciel utilisé : {{ logiciel }}</slot>
+        </div>
+        <div class="modal__body">
+          <slot name="link">{{ link }}</slot>
+        </div>
+        <div class="modal__footer">
+          <slot name="footer">
+            <button class="modal__button" @click="$emit('close')">OK</button>
+          </slot>
+        </div>
+      </div>
+    </div>
+  </Transition>
 </template>
 
 <script setup>
-import { stringifyQuery } from 'vue-router';
-
-defineProps {
-    image: Image,
-    titre:String,
-  date:String,
-  techno:String,
-  logiciel:String,
-  lien:String,
-
-}
+const props = defineProps({
+  show: Boolean,
+  title: String,
+  lang: String,
+  logiciel: String,
+  date: String,
+  link: String,  
+});
 </script>
+
+<style>
+.modal__mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  transition: opacity 0.3s ease;
+}
+
+.modal__container {
+  width: 300px;
+  margin: auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+}
+
+.modal__header {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal__body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+.modal-enter-from {
+  opacity: 0;
+}
+
+.modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-from .modal-container,
+.modal-leave-to .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
